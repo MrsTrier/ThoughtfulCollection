0d0a5c381b22b5117626c8088447382aad9c8c6f
Cheremina Daria <d.cheremina@gmail.com>
2024-11-27 17:10:05 +0100

    title

--- ThoughtfulCollection/ThoughtfulCollectionViewController.swift
+++ ThoughtfulCollection/ThoughtfulCollectionViewController.swift
@@ -35,6 +35,8 @@ class ThoughtfulCollectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Collection"
+        navigationController?.navigationBar.prefersLargeTitles = true
         setUpCollectionView()
     }
 
